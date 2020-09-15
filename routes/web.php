6534de7271ef286c27e04ee6ca596bbabbaf6731
remonhasan <remonhasan041@gmail.com>
2020-09-15 21:26:31 +0600

    updated

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/','HomeController@index')->name('home');
+
 
 Auth::routes();
 
@@ -22,6 +21,9 @@ Route::post('subscriber','SubscriberController@store')->name('subscriber.store')
 Route::group(['as'=>'admin.','prefix'=>'admin','namespace'=>'Admin','middleware'=>['auth','admin']], function (){
 
     Route::get('dashboard','DashboardController@index')->name('dashboard');
+    Route::get('settings','SettingsController@index')->name('settings');
+    Route::put('profile-update','SettingsController@updateProfile')->name('profile.update');
+    Route::put('password-update','SettingsController@updatePassword')->name('password.update');
     Route::resource('tag','TagController');
     Route::resource('category','CategoryController');
     Route::resource('post','PostController');
